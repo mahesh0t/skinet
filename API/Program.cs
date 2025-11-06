41e7b98c5a19a5e5f2a0dc6f8dd9746db3821ece
Mahesh Thiragati <dev.mahesh.t@gmail.com>
2025-11-06 12:32:08 +0530

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using Infrastructure.Data;
 using Core.Interfaces;
+using API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,12 +14,14 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 try
