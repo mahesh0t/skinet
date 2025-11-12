36f0b8e1c43da932560d24c72cb617516543fd99
Mahesh Thiragati <dev.mahesh.t@gmail.com>
2025-11-12 17:34:28 +0530

    end of 10

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import{MatBadge} from "@angular/material/badge";
 import{MatButton} from "@angular/material/button";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
-  ],
+    MatBadge,
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
